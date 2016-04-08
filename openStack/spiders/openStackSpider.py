88fcd7ccddeb969cda9d088e4776fc19319ec7a0
Lazar-T <lazartelebak@gmail.com>
2016-04-08 11:14:49 +0200

    refactored, added testing, and updated code

--- openStack/spiders/openStackSpider.py
+++ openStack/spiders/openStackSpider.py
@@ -1,42 +1,52 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from openStack.items import ItemloadItem
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.contrib.loader import ItemLoader
-from scrapy.contrib.loader.processor import Compose, MapCompose
-from w3lib.html import replace_escape_chars, remove_tags
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.http import Request
-from scrapy.selector import Selector
 import urlparse
+
+from scrapy.http import Request
+from scrapy.loader import ItemLoader
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader.processors import Compose, MapCompose
+from w3lib.html import replace_escape_chars, remove_tags
+
+from openStack.items import ItemloadItem
 
 
 class ItemspiderSpider(CrawlSpider):
-    name = "itemspider"
-    allowed_domains = ["openstacksummitnovember2014paris.sched.org"]
+    name = 'openstack'
+    allowed_domains = ['openstacksummitnovember2014paris.sched.org']
     start_urls = ['http://openstacksummitnovember2014paris.sched.org/directory/attendees/']
-    
+
     rules = (
-        Rule(SgmlLinkExtractor(allow=('/directory/attendees/\d+')), callback='parse_page', follow=True),
-    )    
+        Rule(LinkExtractor(allow=('/directory/attendees/\d+')),
+             callback='parse_page', follow=True),)
 
     def parse_page(self, response):
-        hxs = Selector(response)
-        item_selector = hxs.xpath('//h2/a/@href').extract()
-        for url in item_selector:
-            yield Request(urlparse.urljoin(response.url, url),
+        """Yields all attendee urls.
+
+        @url http://openstacksummitnovember2014paris.sched.org/directory/attendees/
+        @scrapes attendees
+
+        """
+        attendees = response.xpath('//h2/a/@href').extract()
+        for attendee in attendees:
+            yield Request(urlparse.urljoin(response.url, attendee),
                           callback=self.parse_item)
 
-
-
     def parse_item(self, response):
+        """ Returns fields from each individual attendee.
+
+        @url http://openstacksummitnovember2014paris.sched.org/cfb
+        @scrapes name image_url friends title_company_location links about
+
+        """
         l = ItemLoader(item=ItemloadItem(), response=response)
         l.default_output_processor = MapCompose(lambda v: v.strip(), replace_escape_chars)
+
         l.add_xpath('name', '//*[@id="sched-page-me-name"]/text()')
         l.add_xpath('image_url', '//*[@id="myavatar"]/@src')
         l.add_xpath('friends', '//*[@id="sched-page-me-connections"]/ul/li/a/@title')
         l.add_xpath('title_company_location', '//*[@id="sched-page-me-profile-data"]/text()')
         l.add_xpath('links', '//*[@class="sched-network-link"]/a/@href')
         l.add_xpath('about', '//*[@id="sched-page-me-profile-about"]/text()')
+
         return l.load_item()
